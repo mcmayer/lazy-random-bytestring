bd58f2b09cd7ac4c297ff37d9f9c07f055531020
MM <mmayer@mayeranalytics.com>
2018-07-17 08:10:05 +0200

    better Makefile

--- Main.hs
+++ Main.hs
@@ -10,28 +10,31 @@ import           System.IO                    (hPutStrLn, stderr, stdout)
 import           System.Random
 
 -- |Version 1: Recurse with cons
-lazyRandomByteString :: Int -> StdGen -> BSL.ByteString
-lazyRandomByteString n g = fst3 $ iter (BSL.empty, n, g) where
+lazyRandomByteString1 :: Int -> StdGen -> BSL.ByteString
+lazyRandomByteString1 n g = fst3 $ iter (BSL.empty, n, g) where
     fst3 (a, _, _) = a
+    {-# INLINE iter #-}
     iter (bs', n', g') =
         if n' == 0 then (bs', 0, g')
         else iter (w `BSL.cons` bs', n'-1, g'') where
             (w, g'') = random g' :: (Word8, StdGen)
 
 -- |Version 2: Recurse with unfoldr
-lazyRandomByteString' :: Int -> StdGen -> BSL.ByteString
-lazyRandomByteString' n g = BSL.unfoldr f (n, g) where
+lazyRandomByteString2 :: Int -> StdGen -> BSL.ByteString
+lazyRandomByteString2 n g = BSL.unfoldr f (n, g) where
+    {-# INLINE f #-}
     f (n', g') =
         if n' == 0 then Nothing
         else Just (w, (n'-1, g'')) where
             (w, g'') = random g' :: (Word8, StdGen)
 
--- |Version 3: Use `Builder`
-lazyRandomByteString'' :: Int -> StdGen -> BSL.ByteString
-lazyRandomByteString'' n g = toLazyByteString $ builder where
+-- |Version 3: Use [`Builder`](https://hackage.haskell.org/package/bytestring-0.10.0.2/docs/Data-ByteString-Lazy-Builder.html#t:Builder)
+lazyRandomByteString3 :: Int -> StdGen -> BSL.ByteString
+lazyRandomByteString3 n g = toLazyByteString $ builder where
     builder :: Builder
     builder = fst3 $ iter (mempty, n, g) where
         fst3 (a, _, _) = a
+        {-# INLINE iter #-}
         iter :: (Builder, Int, StdGen) -> (Builder, Int, StdGen)
         iter (b, n', g') =
             if n' == 0 then (b, 0, g')
@@ -44,4 +47,4 @@ main = do
     when (null args) $ hPutStrLn stderr "How many bytes do you want?" >> exitFailure
     let len = read (head args) :: Int
     gen <- getStdGen
-    BSL.hPutStr stdout (lazyRandomByteString'' len gen)
+    BSL.hPutStr stdout (lazyRandomByteString3 len gen)
--- Makefile
+++ Makefile
@@ -1,8 +1,12 @@
 lazy-random-bytestring:
 	stack build
 
-prof: build-profile
-	stack exec -- lazy-random-bytestring 1000000 +RTS -p | wc -c
+prof: lazy-random-bytestring.prof
+
+lazy-random-bytestring.prof: build-profile
+	stack exec -- lazy-random-bytestring 1000000 +RTS -p | wc -c && head $@ | grep 'total alloc'
 
 build-profile:
 	stack build --profile  
+
+.PHONY: lazy-random-bytestring prof build-profile
